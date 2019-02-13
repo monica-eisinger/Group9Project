b1081c17bf8912a4dd64e8e1e2d741c9d2292200
Monica Eisinger <47388764+monica-eisinger@users.noreply.github.com>
2019-02-13 09:26:12 -0500

    Add files via upload

---
+++ SickoSearch/SickoSearch/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  SickoSearch
+//
+//  Created by Monica Eisinger on 2/13/19.
+//  Copyright © 2019 Monica Eisinger. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
